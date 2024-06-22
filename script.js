3eadf60d110f518b6341541b263312e229774e40
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-22 15:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getFormvalue() {
-    //Write your code here
 
+function getFormvalue(e) {
+    //Write your code here
+	    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get the form element
+    var form = document.getElementById('form1');
+
+    // Get the values of the input fields
+    var firstName = form.elements['fname'].value;
+    var lastName = form.elements['lname'].value;
+
+    // Display the values using an alert
+    alert(firstName + ' ' + lastName);
 }
